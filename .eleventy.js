e74570be49fe005833fcc74a481519ad4a9598d6
playdougher <zhihao@d-thinker.org>
2024-07-11 16:14:29 +0800

    update content

--- .eleventy.js
+++ .eleventy.js
@@ -25,7 +25,7 @@ module.exports = function (eleventyConfig) {
         htmlTemplateEngine: 'njk',
         dir: {
             input: 'src',
-            output: 'dist'
+            output: '_site'
         }
     };
 };
