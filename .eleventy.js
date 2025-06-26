55a634cf40cacce954b8e2426a382ad1d4bf0f41
playdougher <952331408@qq.com>
2025-06-26 22:51:27 +0800

    add tmp

--- .eleventy.js
+++ .eleventy.js
@@ -23,6 +23,7 @@ module.exports = function (eleventyConfig) {
     
     // Set directories to pass through to the dist folder
     eleventyConfig.addPassthroughCopy('./src/images/');
+    eleventyConfig.addPassthroughCopy('./src/tmp/');
     
     // eleventyConfig.setFrontMatterParsingOptions({
     //     engines: {
